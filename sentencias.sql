117ab47447922322df9a5204fbc3bf8e974b8d64
VictorAnizar <victoranizarmorales@gmail.com>
2021-07-10 11:07:49 -0500

    Usando WHERE

--- sentencias.sql
+++ sentencias.sql
@@ -41,3 +41,40 @@ select * from sakila.payment;
 select distinct(amount) from sakila.payment order by amount asc;
 /*or*/
 select min(amount) as cantidad_baja, max(amount) as cantidad_alta from sakila.payment;
+
+/****************************End of set 1 of exercises *******************************/
+
+/*WHERE: provide a filter in a SELECT sentence based on a determined value*/
+
+/*Shows all the rows which column first_name contains the word 'DAN'*/
+select * from sakila.actor where first_name='DAN';
+/*Shows all the rows which column city contains the word 'London'*/
+select * from sakila.city where city='London';
+/*Shows all the rows which column country_id contains the number 102*/
+select * from sakila.city where country_id=102;
+/*Shows all the rows which column store_id contains the number 1*/
+select * from sakila.customer where store_id=1;
+/*Shows all the rows which column film_id contains a greather number than 50*/
+select * from sakila.inventory where film_id>50;
+/*Shows the rows which column amount contains a uniques minor numbers than 3*/
+select distinct(amount) from sakila.payment where amount<3;
+/*Shows all the rows which column staff_id  is different than 2*/
+select * from sakila.staff where staff_id<>2;
+/*Shows all the rows which column name is different than 'German'*/
+select * from sakila.language where name<>'German';
+
+/********************Exercices using WHERE************************/
+
+/*1.- Consulta description, release_year de la tabla film de la base de datos sakila.*/
+select description, release_year from sakila.film;
+
+/*2- Filtra la información donde title sea IMPACT ALADDIN*/
+select * from sakila.film where title='IMPACT ALADDIN';
+
+/*3- Consulta la tabla payment de la base de datos sakila.*/
+select * from sakila.payment;
+
+/*4- Filtra la información donde amount sea mayor a 0.99.*/
+select * from sakila.payment where amount>0.99 order by amount asc;
+
+/****************************End of set 2 of exercises *******************************/
